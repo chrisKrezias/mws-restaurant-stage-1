d80617e42d10f9e9070a7c5c9b6d0a8fb83597cf
Chris Krezias <chris.krezias@aqurance.com>
2018-04-01 22:03:26 +0300

    fix: service worker running script added to restaurant info page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -202,3 +202,15 @@ getParameterByName = (name, url) => {
         return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
